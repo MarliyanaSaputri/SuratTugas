6f4dd2a5a46ca9f06328b7ce1f5423d7bafe78bf
MarliyanaSaputri <50563432+MarliyanaSaputri@users.noreply.github.com>
2019-10-10 08:56:56 +0700

    Menambah Model
    Putri menambah Model CI dalam folder Models

---
+++ application/models/daftar_model.php
@@ -0,0 +1,61 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Daftar_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+	//mengambil data lha//
+
+	  public function daftar_view($tabel) 
+	 {
+		 
+		  $query = $this->db->get($tabel);
+        return $query->result_array();
+		 
+	 }
+	 public function insert_data($data,$table)
+	{
+	$this->db->insert($table,$data);
+    }
+    function cari_data($tabel,$where)
+	{
+		$query = $this->db->get_where($tabel,$where) ;
+		return $query->result_array();
+    }
+	public function get_jb(){
+		
+			$query = $this->db->query("SELECT * from tb_pegawai WHERE jabatan = 'Kepala Bidang Aplikasi Informatika'");
+			return $query->result_array();
+		}
+
+		
+		public function get_j($nip){
+		
+			$query = $this->db->query("SELECT * FROM 'spt'.'tb_pegawai' WHERE 'NIP' = $nip ORDER BY `jabatan` DESC");
+			return $query->result_array();
+		}
+
+  	public function get_all($tabel,$tabeljoin,$wherejoin){
+		
+			$query = $this->db->query("select * from $tabel jm JOIN $tabeljoin m ON m.$wherejoin = jm.$wherejoin");
+			return $query->result_array();
+		}
+	
+	public function get_cariall($tabel,$tabeljoin,$wherejoin,$where){
+		
+			$query = $this->db->get_where("$tabel jm JOIN $tabeljoin m ON m.$wherejoin = jm.$wherejoin where id_st = '$where'");
+			return $query->result_array();
+		}
+	public function update_data($where,$data,$table){
+	$this->db->where($where);
+	$this->db->update($table,$data);
+    }  
+	 
+	public function delete_data($where,$table){
+	$this->db->where($where);
+	$this->db->delete($table);
+    }   
+
+	}
