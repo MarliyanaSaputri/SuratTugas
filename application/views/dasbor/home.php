0c890cb6ff350c9e6696ddcce1b22761ca16fc00
MarliyanaSaputri <50563432+MarliyanaSaputri@users.noreply.github.com>
2019-10-10 09:34:57 +0700

    menambah folder dasbor
    folder dasbor pada view yang berisi home.php

---
+++ application/views/dasbor/home.php
@@ -0,0 +1,12 @@
+ <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>DISKOMINFO <small>Dinas Komunikasi dan Informatika Provinsi Jawa Timur</small></h1>
+                    <div class="alert alert-success alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                       SELAMAT DATANG
+                       <P>Aplikasi Surat Perintah Tugas</P>
+                    </div>
+                </div>
+            </div>
+            </div>
---
+++ application/views/dasbor/konten.php
@@ -0,0 +1,11 @@
+ <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Dashboard <small>Dashboard Home</small></h1>
+                    <div class="alert alert-success alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        Welcome to the admin dashboard! Feel free to review all pages and modify the layout to your needs. 
+                        For the purpose of data visualization, additional widgets are used, so make sure you review all provided information.
+                    </div>
+                </div>
+            </div>
