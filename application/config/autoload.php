16cbd785814d91ac4b405f36768d24d47d62116f
bay07 <50563318+bay07@users.noreply.github.com>
2019-10-11 12:45:42 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database' ,'form_validation');
+$autoload['libraries'] = array('database' ,'form_validation','database', 'session', 'fungsi', 'fpdf','PDF_Code128');
 
 
 /*
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form');
+$autoload['helper'] = array('url','form','fungsi');
 
 /*
 | -------------------------------------------------------------------
