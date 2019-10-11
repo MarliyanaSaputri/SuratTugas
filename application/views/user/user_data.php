e5f51c05b6919f75820974d62075f88148842312
bayu samudera <bayusamudera9a@gmail.com>
2019-10-11 13:43:21 +0700

    tambah view user

---
+++ application/views/user/user_data.php
@@ -0,0 +1,101 @@
+<div class="row">
+	
+	<section class="content-header">
+		
+                <h1>User
+                </h1>
+                <ol class="breadcrumb">
+                	<li><a href=""><i class="fa fa-user"></i></a></li>
+                	<li class="active">User</li>
+                </ol>
+
+	</section>
+
+
+        <!--main Content-->
+
+        <section class="content">
+        
+         <div class="col-xs-12">
+        <div class="box">
+                <div class="box-header">
+                        <h3 class="box-title text-center">Data User</h3>
+                        <div class="pull-right">
+                                <a href="<?=site_url('user/add')?>" class="btn btn-primary btn-flat">
+                                        <i class="fa fa-plus"></i> Create
+                                </a>
+
+                        </div>
+                </div>
+                
+                
+                <br>
+                                        <div class="box-body table-responsive">
+              <table id="example1" class="table table-bordered table-striped table-paginate " cellspacing="" width="100%">
+                                <thead>
+                                        <tr class="center">
+                                                <th class="text-center">No</th>
+                                                <th class="text-center">Username</th>
+                                                <th class="text-center">Nama</th>
+                                                 <th class="text-center">Level</th>
+                                                <th class="text-center">Pilihan</th>
+                                        </tr>
+                                </thead>
+                                <tbody>
+
+                                <?php $no = 1;
+                                foreach ($row->result() as $key => $data) { ?>
+                                        <tr>
+                                                <td class="text-center"><?=$no++?>.</td>
+                                                <td class="text-center"><?=$data->username?></td>
+                                                <td class="text-center"><?=$data->nama?></td>
+                                                <td class="text-center"><?=$data->level == 1 ? "Administrator" : "User"?></td>
+                                                <td  class="text-center" width="250px">
+
+                                                 <form action="<?=site_url('user/del')?>" method="post">
+
+                                             <a href="<?=site_url('user/edit/'.$data->user_id)?>" class="btn btn-primary btn-sm" >
+                                                        <i class="glyphicon glyphicon-pencil"></i> Update
+                                                </a> 
+                                                        
+                                                  <input type="hidden" name="user_id" value="<?=$data->user_id?>">
+
+                                               <button onclick="return confirm ('Apakah Anda Yakin ?')"  class="btn btn-danger btn-sm" >
+                                                        <i class="glyphicon glyphicon-trash"></i> Delete
+                                                </button> 
+                                              </form>
+                                        </td>
+                                </tr>
+                                <?php
+                        } ?>
+
+                                </tbody>
+                </table>
+</div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+     
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+</div>
+
+<script>
+  
+  $(function () {
+   
+    $('#example1').DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": true
+    });
+  });
+</script>
+
---
+++ application/views/user/user_form_edit.php
@@ -0,0 +1,102 @@
+<div class="page-wrapper">
+<div class="row">
+	
+	<section class="content-header">
+		
+                <h1>User
+                </h1>
+                <ol class="breadcrumb">
+                	<li><a href=""><i class="fa fa-user"></i></a></li>
+                	<li class="active">User</li>
+                </ol>
+
+	</section>
+
+
+        <!--main Content-->
+
+        <section class="content">
+        
+         <div class="col-xs-12">
+        <div class="box">
+                <div class="box-header">
+                        <h3 class="box-title text-center">Edit User</h3>
+                        <div class="pull-right">
+                                <a href="<?=site_url('user')?>" class="btn btn-warning btn-flat">
+                                        <i class="fa fa-undo"></i> Back
+                                </a>
+
+                        </div>
+                </div>
+                
+                
+                <br>
+                                        <div class="box-body">
+
+<div class="row">
+<div class="col-md-4 col-md-offset-4">
+<?php //echo validation_errors(); ?> 
+<form action="" method="post">
+
+<div class="form-group <?=form_error('fullname') ? 'has-error' : null?>">
+	<label>Nama *</label>
+    <input type="hidden" name="user_id" value="<?=$row->user_id?>">
+	<input type="text" name="fullname" value="<?=$this->input->post('fullname') ? : $row->nama?>" class="form-control">
+	<?=form_error('fullname')?>
+</div>
+<div class="form-group <?=form_error('username') ? 'has-error' : null?>">
+	<label>Username *</label>
+	<input type="text" name="username" value="<?=$this->input->post('username') ? : $row->username?>" class="form-control">
+	<?=form_error('username')?>
+</div>
+<div class="form-group <?=form_error('password') ? 'has-error' : null?>">
+	<label>Password </label> <small>biarkan kosong jika tidak diganti</small>
+	<input type="password" name="password" value="<?=$this->input->post('password')?>" class="form-control">
+	<?=form_error('password')?>
+</div>
+<div class="form-group <?=form_error('passconf') ? 'has-error' : null?>">
+	<label>Password Confirmation </label>
+	<input type="password" name="passconf" value="<?=$this->input->post('passconf')?>" class="form-control">
+	<?=form_error('passconf')?>
+</div>
+<div class="form-group <?=form_error('level') ? 'has-error' : null?>">
+	<label>Level </label>
+	<select name="level" class="form-control">
+		<?php $level = $this->input->post('level') ? $this->input->post('level') : $row->level ?>
+		<option value="1"> Admin</option>
+		<option value="2" <?=$level == 2 ? "selected" : null?>> User</option>
+</select>
+<?=form_error('level')?>
+</div>
+<div class="form-group">
+	<button type="submit" class="btn btn-success btn-flat"><i class="fa fa-play"></i> Save</button>
+	<button type="reset" class="btn btn-danger btn-flat"><i class="fa fa-undo"></i> Reset</button>
+</div>
+
+
+</div>
+
+</form>
+
+
+</div>
+
+</div>
+
+
+              
+</div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+     
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+</div>
+
+</div>
+
