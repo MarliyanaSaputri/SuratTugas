f4f01632fddffa559c40375f6770aaecf136e957
AgustinTri <50563422+AgustinTri@users.noreply.github.com>
2019-10-14 10:13:05 +0700

    edit form tambah DL
    edit form tambah DL

--- application/views/dinasluar/formtmb.php
+++ application/views/dinasluar/formtmb.php
@@ -62,7 +62,7 @@
 							                Maksud dan Tujuan
 							            </label>
 					                  <div class="col-sm-5">
-					                    <input type="text" class="form-control" name="maksud_tujuan" placeholder="">
+					                    <textarea type="text" class="form-control" name="maksud_tujuan" placeholder=""></textarea>
 					                  </div>
 					                </div>
 
@@ -71,7 +71,7 @@
 							                Tanggal Pelaksanaan 
 							            </label>
 					                  <div class="col-sm-5">
-					                    <input type="text" id="range-tanggal" class="form-control" placeholder="dd/mm/yyyy - dd/mm/yyyy">
+					                    <input type="text" id="range_tgl" name="tgl_pelaksanaan" class="form-control" placeholder="dd/mm/yyyy - dd/mm/yyyy">
             									
 					                  </div>
 					                  
@@ -89,7 +89,7 @@
 					                  <div class="col-sm-5">
 					                  	<input id="hadir" value="1" type="hidden" />
 									    <button type="button" onclick="tambahPeserta(); return false;"><span class="glyphicon glyphicon-plus"></span></button>
-									    <div id="divHobi"></div>
+									    <div id="divPeserta"></div>
 					                  </div>
 					                </div>
 
@@ -129,28 +129,7 @@
 					                  </div>
 					                </div>
 
-									<!-- <div class="form-group">
-		                                <label for="firstname" class="col-md-3">
-		                                 Penanggungjawab</label>
-		                                <div class="col-sm-5">
-		                                    <input type="text" class="form-control" name="nama" id="nama" placeholder="">
-		                                </div>
-		                            </div>
-
-					                <div class="form-group">	 
-							            <label for="firstname" class="col-md-3">
-	                                        NIP 
-	                                    </label>
-	                                     <div class="col-md-5">
-	                                    <input type="text" class="form-control" name="nip" id="nip" placeholder="" readonly="" />
-										</div>
-										<div class="col-md-2">
-		                                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">
-		                                        <span class="glyphicon glyphicon-search"></span>
-		                                    </button>
-		                                </div>
-									</div> -->
-	</div>
+								</div>
 									
 									<div id="insert-form"></div>
 					                <div class="box-footer">
@@ -165,92 +144,37 @@
 				           </div> 
 
 					                            
-				           <!-- Modal -->
-							<!-- <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					            <div class="modal-dialog" style="width:800px">
-					                <div class="modal-content">
-					                    <div class="modal-header">
-					                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					                        <h4 class="modal-title" id="myModalLabel">Lookup Pegawai</h4>
-					                    </div>
-					                    <div class="modal-body">
-					                        <table id="lookup" class="table table-bordered table-hover table-striped">
-					                            <thead>
-					                            <tr>
-					                                <th>NIP</th>
-					                                <th>Nama</th>                
-					                            </tr>
-					                            </thead>
-					                            <tbody>
-					                            <?php
-					                            $no  = 1;
-					                            foreach ($daftarpegawai as $peg) :
-					                            ?>
-					                            <tr class="pilih" 
-					                            data-nip="<?php echo $peg['nip']; ?>"
-					                            data-nama="<?php echo $peg['nama']; ?>">
-					                           <td><?php echo $peg['nip']; ?></td>
-					                            <td><?php echo $peg['nama']; ?></td>                                    
-					                            </tr>
-					                            <?php
-					                            endforeach;
-					                            ?>
-					                               
-					                            </tbody>
-					                        </table>  
-					                    </div>
-					                </div>
-					            </div>
-					        </div>   
-					        <script type="text/javascript">
-					//            jika dipilih, NIP akan masuk ke input dan modal di tutup
-					            $(document).on('click', '.pilih', function (e) {
-					                document.getElementById("NIP").value = $(this).attr('data-nip');
-					                document.getElementById("nama").value = $(this).attr('data-nama');
-					                $('#myModal').modal('hide');
-					            });
-					//            tabel lookup mahasiswa
-					            $(function () {
-					                $("#lookup").dataTable();
-					            });
-					            // function dummy() {
-					            //     var NIP = document.getElementById("NIP").value;
-					            //     alert('Nomor Induk Mahasiswa ' + NIP + ' berhasil tersimpan');
-					                
-					            //     var ket = document.getElementById("ket").value;
+				          
 
-					            //     alert('Keterangan ' + ket + ' berhasil tersimpan');
-					           // }
-					        </script> -->
+							<script src="<?php echo base_url();?>asset/js/moment.js"></script>
+						    <!-- js untuk daterangepicker -->
+						    <script src="<?php echo base_url();?>asset/js/daterangepicker.js"></script>
 
- <script src="<?php echo base_url();?>asset/js/moment.js"></script>
-    <!-- js untuk daterangepicker -->
-    <script src="<?php echo base_url();?>asset/js/daterangepicker.js"></script>
+						    <script type="text/javascript">
+						        $(document).ready(function(){
+						            $('.range-dtp i').click(function() {
+						              $(this).parent().find('input').click();
+						            });
 
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('.range-dtp i').click(function() {
-              $(this).parent().find('input').click();
-            });
+						             $('#range_tgl').daterangepicker({
+						                autoUpdateInput: false,
+						                format : 'DD/MM/YYYY',
+						                useCurrent : false,
+						                "showDropdowns": true,
+						                "autoApply": true,
+						             }, function(start, end, label) {
+						                $('#range_tgl').val(start.format('DD/MM/YYYY')+" - "+end.format('DD/MM/YYYY'))
+						            });
 
-             $('#range-tanggal').daterangepicker({
-                autoUpdateInput: false,
-                format : 'DD/MM/YYYY',
-                useCurrent : false,
-                "showDropdowns": true,
-                "autoApply": true,
-             }, function(start, end, label) {
-                $('#range-tanggal').val(start.format('DD/MM/YYYY')+" - "+end.format('DD/MM/YYYY'))
-            });
+						        });
+						    </script>
 
-        });
-    </script>
 							<script language="javascript">
 							   function tambahPeserta() {
 							     var hadir = document.getElementById("hadir").value;
 							     var stre;
 							     stre="<p id='srow" + hadir + "'><input type='text' size='45' name='peserta_hadir' placeholder='Nama Peserta' /> <a href='#' style=\"color:#3399FD;\" onclick='hapusElemen(\"#srow" + hadir + "\"); return false;'>Hapus</a></p>";
-							     $("#divHobi").append(stre);
+							     $("#divPeserta").append(stre);
 							     hadir = (hadir-1) + 2;
 							     document.getElementById("hadir").value = hadir;
 							   }
--- application/views/dinasluar/formtmb1.php
+++ application/views/dinasluar/formtmb1.php
@@ -11,7 +11,7 @@
                         <div class="col-lg-12">
                          <div class="panel panel-warning">
                             <div class="panel-heading">
-                            <h3 class="panel-title"><i class="fa fa-bar-chart-o"></i> Form Edit Dinas Luar</h3>
+                            <h3 class="panel-title"><i class="fa fa-bar-chart-o"></i> Form Tambah Dinas Luar</h3>
                             </div>
                             <div class="panel-body">
 						    <!-- Main content -->
@@ -19,8 +19,9 @@
 						      <div class="row">
 								<div class="col-md-12">
             					<!-- form start -->
-            					<?php foreach($daftardl as $dl): ?>
-					            <form class="form-horizontal" method="post" id="formedit" action="<?php echo base_url()?>DinasLuar/proses_edit/<?php echo $dl['id_dl'] ;?>">
+            					<?php foreach($daftarst as $st): ?>
+					            <form class="form-horizontal" method="post" id="formtmb" action="<?php echo base_url()?>DinasLuar/prosestambah/<?php echo $st['id_st'] ;?>">
+
 					           	<div class="box-body">
 								
 					           		<div class="form-group">
@@ -28,7 +29,7 @@
 							                ID Dinas Luar : 
 							            </label>
 					                  <div class="col-sm-5">
-					                    <input type="text" class="form-control" value ="<?php echo $dl['id_dl']; ?>" name="id_dl" id="id_dl" placeholder="" readonly="">
+					                    <input type="text" class="form-control" name="id_dl" placeholder="" readonly="" />
 					                  </div>
 					                </div>
 
@@ -37,7 +38,7 @@
 						                	No. ST : 
 						            	</label>
 										<div class="col-sm-5">
-										 <input type="text" class="form-control" value ="<?php echo $dl['no_st']; ?>" name="no_st" id="no_st" placeholder="" readonly="" />
+										 <input type="text" class="form-control" value="<?php echo $st['no_st']; ?>" name="no_st" placeholder="" readonly=""/>
 										</div>
 									</div>
 
@@ -46,7 +47,7 @@
 	                                        NIP 
 	                                    </label>
 	                                     <div class="col-md-5">
-	                                    	<input type="text" class="form-control" value ="<?php echo $dl['NIP']; ?>" name="NIP" id="NIP" placeholder="" readonly="" />
+	                                    	<input type="text" class="form-control" value="<?php echo $st['NIP']; ?>" name="NIP" placeholder="" readonly="" />
 										</div>
 									</div>
 
@@ -55,17 +56,21 @@
 							                Maksud dan Tujuan
 							            </label>
 					                  <div class="col-sm-5">
-					                    <textarea type="text" class="form-control" name="maksud_tujuan" id="maksud_tujuan" placeholder=""><?php echo $dl['maksud_tujuan']; ?></textarea>
+					                    <input type="text" class="form-control" name="maksud_tujuan" placeholder="">
 					                  </div>
 					                </div>
 
-									<div class="form-group">
+					                <div class="form-group">
 					                   <label for="firstname" class="col-md-3">
 							                Tanggal Pelaksanaan 
 							            </label>
-					                  <div class="col-sm-3">
-					                    <input type="text" class="form-control datepicker" value ="<?php echo date("d-m-Y",strtotime($dl['tgl_pelaksanaan']))?>" id="range_tgl" name="tgl_pelaksanaan">
+					                  <div class="col-sm-5">
+					                    <input type="text" id="range_tgl" name="tgl_pelaksanaan" class="form-control" placeholder="dd/mm/yyyy - dd/mm/yyyy">
+            									
 					                  </div>
+					                  <!-- <div class="col-sm-2">
+					                    <input type="date" class="form-control datepicker" name="tgl_akhir">
+					                  </div> -->
 					                </div>
 
 									<div class="form-group">
@@ -73,20 +78,19 @@
 							                Peserta yang Hadir 
 							            </label>
 					                  <div class="col-sm-5">
-					                  	<!-- <input type="text" class="form-control" value="<?php// echo $dl['peserta_hadir']; ?>" name="peserta_hadir" placeholder="1. "> -->
-					                 	<input id="hadir" value="<?php echo $dl['peserta_hadir']; ?>" type="text" />
-					                    <input id="hadir" value="1" type="hidden" />
+					                  	<input id="hadir" value="1" type="hidden" />
 									    <button type="button" onclick="tambahPeserta(); return false;"><span class="glyphicon glyphicon-plus"></span></button>
 									    <div id="divPeserta"></div>
 					                  </div>
 					                </div>
+
 
 				                	<div class="form-group">
 					                   <label for="firstname" class="col-md-3">
 							                Daerah Tujuan 
 							            </label>
 					                  <div class="col-sm-5">
-					                    <input type="text" class="form-control" value ="<?php echo $dl['daerah_tujuan']; ?>" name="daerah_tujuan" id="daerah_tujuan" placeholder="">
+					                    <input type="text" class="form-control" name="daerah_tujuan" placeholder="">
 					                  </div>
 					                </div>
 
@@ -95,7 +99,7 @@
 							                Bahasan 
 							            </label>
 					                  <div class="col-sm-5">
-					                    <textarea type="text" class="form-control" name="bahasan" id="bahasan" placeholder=""><?php echo $dl['bahasan']; ?></textarea>
+					                    <textarea type="text" class="form-control" name="bahasan" placeholder=""></textarea>
 					                  </div>
 					                </div>
 
@@ -104,7 +108,7 @@
 							                Lain - Lain 
 							            </label>
 					                  <div class="col-sm-5">
-					                    <textarea type="text" class="form-control" name="lain-lain" id="lain-lain" placeholder=""><?php echo $dl['lain-lain']; ?></textarea>
+					                    <textarea type="text" class="form-control" name="lain-lain" placeholder=""></textarea>
 					                  </div>
 					                </div>
 								
@@ -113,54 +117,31 @@
 							                Tanggal Pembuatan
 							            </label>
 					                  <div class="col-sm-5">
-					                    <input type="text" class="form-control datepicker" value="<?php echo $dl['tgl_pembuatan']; ?>" name="tgl_pembuatan" placeholder="">
-					                    <!-- value="<?php //echo date("d-m-Y",strtotime($dl['tgl_pembuatan']))?>" -->
+					                    <input type="text" class="form-control datepicker" name="tgl_pembuatan" id="datepicker" placeholder="">
 					                  </div>
 					                </div>
 
-							
+								
+									<div id="insert-form"></div>
 					                <div class="box-footer">
-						                <button type="submit" name="simpan" class="btn btn-info pull-right">Edit</button>
+						                <button type="submit" name="simpan" class="btn btn-info pull-right">Tambah</button>
 					              	</div>
 				              	</div>
 				              </form>
-				           
-	                        <?php
-	                        endforeach;
-	                        ?>
+							  <?php
+	                            endforeach;
+	                          ?>
 							 </div>
 				           </div> 
 
 
 
-				           <script src="<?php echo base_url();?>asset/js/moment.js"></script>
-						    <!-- js untuk daterangepicker -->
-						    <script src="<?php echo base_url();?>asset/js/daterangepicker.js"></script>
-
-						    <script type="text/javascript">
-						        $(document).ready(function(){
-						            $('.range-dtp i').click(function() {
-						              $(this).parent().find('input').click();
-						            });
-
-						             $('#range_tgl').daterangepicker({
-						                autoUpdateInput: false,
-						                format : 'DD/MM/YYYY',
-						                useCurrent : false,
-						                "showDropdowns": true,
-						                "autoApply": true,
-						             }, function(start, end, label) {
-						                $('#range_tgl').val(start.format('DD/MM/YYYY')+" - "+end.format('DD/MM/YYYY'))
-						            });
-
-						        });
-						    </script>
 
 							<script language="javascript">
 							   function tambahPeserta() {
 							     var hadir = document.getElementById("hadir").value;
 							     var stre;
-							     stre="<p id='srow" + hadir + "'><input type='text' size='45' name='peserta_hadir' placeholder='Nama Peserta' /> <a href='#' style=\"color:#3399FD;\" onclick='hapusElemen(\"#srow" + hadir + "\"); return false;'>Hapus</a></p>";
+							     stre="<p id='srow" + hadir + "'><input type='text' size='45' name='peserta_hadir' placeholder=' Nama Peserta ' /> <a href='#' style=\"color:#3399FD;\" onclick='hapusElemen(\"#srow" + hadir + "\"); return false;'>Hapus</a></p>";
 							     $("#divPeserta").append(stre);
 							     hadir = (hadir-1) + 2;
 							     document.getElementById("hadir").value = hadir;
@@ -171,7 +152,35 @@
 							</script>
 
 
-						</div>				            
+
+							<script src="<?php echo base_url();?>asset/js/moment.js"></script>
+						    <!-- js untuk daterangepicker -->
+						    <script src="<?php echo base_url();?>asset/js/daterangepicker.js"></script>
+
+
+						    <script type="text/javascript">
+						        $(document).ready(function(){
+						            $('.range-dtp i').click(function() {
+						              $(this).parent().find('input').click();
+						            });
+
+						             $('#range_tgl').daterangepicker({
+						                autoUpdateInput: true,
+						                format : 'DD/MM/YYYY',
+						                useCurrent : false,
+						                "showDropdowns": true,
+						                "autoApply": true,
+						             }, function(start, end) {
+						                $('#range_tgl').val(start.format('DD/MM/YYYY')+" - "+end.format('DD/MM/YYYY'))
+						            });
+
+						        });
+						    </script>
+
+							
+
+							 
+				       </div>				            
 				    </div>
 				</div>
 			</div>
