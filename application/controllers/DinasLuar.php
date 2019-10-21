4bc6a6c6e8d43b81246a7d30adf4d8900671d114
AgustinTri <50563422+AgustinTri@users.noreply.github.com>
2019-10-21 09:05:10 +0700

    nambah daterange pada edit DL

--- application/controllers/DinasLuar.php
+++ application/controllers/DinasLuar.php
@@ -146,9 +146,9 @@ class  DinasLuar extends CI_Controller {
 		$range_tgl = $this->input->post('tgl_pelaksanaan');
 		$range_tgl =  substr($range_tgl,6,4) . '-'.substr($range_tgl,3,2) . 
 	'-'.substr($range_tgl,0,2) ;
-	//	$range_tgl = $this->input->post('tgl_akhir');
-	//	$range_tgl =  substr($range_tgl,6,4) . '-'.substr($range_tgl,3,2) . 
-	//'-'.substr($range_tgl,0,2) ;
+		$range_tgl2 = $this->input->post('tgl_pelaksanaan');
+		$range_tgl2 =  substr($range_tgl2,17,4) . '-'.substr($range_tgl2,14,2) . 
+	'-'.substr($range_tgl2,11,2) ;
 		$hadir = $this->input->post('peserta_hadir');
 		$hadir2 = $this->input->post('hadir2');
 		$hadir3 = $this->input->post('hadir3');
@@ -166,7 +166,7 @@ class  DinasLuar extends CI_Controller {
 			'NIP' => $nip,
 			'maksud_tujuan' => $maksud,
 			'tgl_pelaksanaan' => $range_tgl,
-			//'tgl_akhir' => $range_tgl,
+			'tgl_akhir' => $range_tgl2,
 			'peserta_hadir' => $hadir,
 			'hadir2' => $hadir2,
 			'hadir3' => $hadir3,
