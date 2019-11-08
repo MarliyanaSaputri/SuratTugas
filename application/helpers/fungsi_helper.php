d18bb0c505f2cdaa3c6a985992ed0d72659b5767
bay07 <50563318+bay07@users.noreply.github.com>
2019-11-08 10:41:51 +0700

    bay update fungsi_helper.php

--- application/helpers/fungsi_helper.php
+++ application/helpers/fungsi_helper.php
@@ -2,14 +2,14 @@
 
 function check_already_login() {
 	$ci =& get_instance();
-	$user_session = $ci->session->userdata('userid');
+	$user_session = $ci->session->userdata('NIP');
 	if($user_session) {
 		redirect('home');
 	}
 }
 function check_not_login() {
 	$ci =& get_instance();
-	$user_session = $ci->session->userdata('userid');
+	$user_session = $ci->session->userdata('NIP');
 	if(!$user_session) {
 		redirect('auth/login');
 	}
